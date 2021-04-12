a7f58e5344e3e556dbb7b0518989f2f034167f3f
Nate <7nater.nelson@gmail.com>
2021-04-12 11:05:26 -0600

    commit tree 1b0d0a5370e27106aa82eef59c772aee541c59ac

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeString){
+  const hour = parseInt(timeString.split(":")[0],10)
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour < 17){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message){
+  const greeting = document.getElementById("greeting");
+  
+  greeting.innerText = message
+  console.log(greeting.innerHTML)
+}
